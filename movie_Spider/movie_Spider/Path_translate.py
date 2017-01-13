ad117e59442cfec907e27bf21a91cce0f7a6c92d
tingyun <13284638588@163.com>
2017-01-13 19:07:43 +0800

    update mangguo_movie crawler.

--- movie_Spider/movie_Spider/Path_translate.py
+++ movie_Spider/movie_Spider/Path_translate.py
@@ -65,7 +65,7 @@ def Get_Valid_Url(urls):
 def get_HeadUrl(index_url,spider_name):
 	if spider_name == "youku_tv":
 			return re.sub(r"\d.html","{page}.html",index_url)
-	if spider_name == "mangguo_tv":
+	if spider_name == "mangguo_movie":
 			return re.sub(r"\d-0--.html","{page}-0--.html",index_url)
 	if spider_name == "souhu_movie":
 			return re.sub(r"\d_p11_p12_p13.html","{page}_p11_p12_p13.html",index_url)
